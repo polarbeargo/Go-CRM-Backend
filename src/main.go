2b274179adfa475c23f51e37cc0aa4688a0336df
Hsin-Wen Chang <bow1226@gmail.com>
2023-12-10 18:17:31 -0500

    feat: Implement Routing

---
+++ src/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var customers = map[string]string{
+	"1": "Andy",
+	"2": "Peter",
+	"3": "Gabriella",
+	"4": "Jordy",
+}
+
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(customers)
+}
+
+func deleteCustomers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id := mux.Vars(r)["id"]
+
+	if _, ok := customers[id]; ok {
+		delete(customers, id)
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(customers)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(customers)
+	}
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/customers", getCustomers).Methods("GET")
+	router.HandleFunc("/deleteMember/{id}", deleteCustomers).Methods("DELETE")
+
+	fmt.Println("Server is starting on port 3000...")
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
